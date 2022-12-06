13755ddc3f7df0ff25d45f7b4097e031021eb60a
YevhenVovk <vovk.valki@ukr.net>
2022-12-06 14:27:57 +0200

    POST method for adding tasks
    create default value for the status;
    add endpoint '/todo/api/v1.0/tasks' with POST method;

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
 import dotenv
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 
 import database
 import models
+
 
 # Loading environment variables into the project
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -21,11 +22,6 @@ def shutdown_session(exception=None) -> None:
     database.db_session.remove()
 
 
-@app.route('/todo/api/v1.0/')
-def hello_world():
-    return 'Hello World!'
-
-
 @app.route('/todo/api/v1.0/board', methods=['GET'])
 def get_boards():
     database.init_db()
@@ -35,9 +31,41 @@ def get_boards():
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET', 'POST'])
 def get_tasks():
+    database.init_db()
     if request.method == "GET":
-        task_list = models.Task.query.all()
+        filter_param = dict(request.values)
+        if 'status' in filter_param:
+            task_list = models.Task.query.filter_by(status=filter_param['status']).all()
+        elif 'board' in filter_param:
+            task_list = models.Task.query.filter_by(board_id=filter_param['board']).all()
+        else:
+            task_list = models.Task.query.all()
+        if not task_list:
+            abort(404)
         return jsonify([item.to_dict() for item in task_list])
+    if request.method == "POST":
+        create_data = dict(request.get_json())
+        new_task = models.Task(
+            text=create_data.get('text'),
+            board_id=create_data.get('board_id')
+        )
+        try:
+            database.db_session.add(new_task)
+            database.db_session.commit()
+        except Exception as error:
+            return jsonify(error=str(error))
+        return jsonify(new_task.to_dict())
+
+
+@app.get('/todo/api/v1.0/tasks/<int:id_task>')
+def get_one_task(id_task: int):
+    database.init_db()
+    get_task = models.Task.query.filter_by(id=id_task).first()
+    if not get_task:
+        abort(404)
+    return jsonify(get_task.to_dict())
+
+
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import enum as lib_enum
 
 from sqlalchemy import Column, Integer, ForeignKey, DateTime, Text, func, Boolean, Enum
 from sqlalchemy.orm import relationship, backref
+from sqlalchemy.sql import expression
 
 from database import Base
 
@@ -19,7 +20,7 @@ class Board(Base):
     modification_data = Column(DateTime(timezone=True), onupdate=func.now())
     status = Column(Enum(BoardStatus))  # ARCHIVED/OPEN
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
         return {
             'id': self.id,
             'creation_date': self.creation_date,
@@ -32,15 +33,15 @@ class Task(Base):
     __tablename__ = 'task'
 
     id = Column(Integer, primary_key=True, nullable=False)
-    creation_date = Column(DateTime(timezone=True), server_default=func.now())
-    modification_data = Column(DateTime(timezone=True), onupdate=func.now())
-    status = Column(Boolean, nullable=False)
+    creation_date = Column(DateTime(), server_default=func.now())
+    modification_data = Column(DateTime(), nullable=True, onupdate=func.now())
+    status = Column(Boolean, server_default='f', default=False)
     text = Column(Text, nullable=False)
     board_id = Column(Integer, ForeignKey('board.id'), nullable=False)
 
     board = relationship('Board', backref=backref('task', lazy=True))
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
         return {
             'id': self.id,
             'creation_date': self.creation_date,
