7f189871a6c99a368c12833314638ae655531af9
YevhenVovk <vovk.valki@ukr.net>
2022-12-06 15:07:01 +0200

    Error 400 if parameters passed are invalid
    check parameters in delete_task, update_task, get_tasks;

--- app.py
+++ app.py
@@ -37,18 +37,24 @@ def get_boards():
 def get_tasks():
     database.init_db()
     if request.method == "GET":
+        task_list = []
         filter_param = dict(request.values)
-        if 'status' in filter_param:
+        if filter_param and 'status' in filter_param:
             task_list = models.Task.query.filter_by(status=filter_param['status']).all()
-        elif 'board' in filter_param:
+        elif filter_param and 'board' in filter_param:
             task_list = models.Task.query.filter_by(board_id=filter_param['board']).all()
         else:
+            abort(400)
+        if not filter_param:
             task_list = models.Task.query.all()
-        if not task_list:
-            abort(404)
         return jsonify([item.to_dict() for item in task_list])
     if request.method == "POST":
         create_data = dict(request.get_json())
+        if not create_data:
+            abort(400)
+        else:
+            if 'text' not in create_data and 'board_id' not in create_data:
+                abort(400)
         new_task = models.Task(
             text=create_data.get('text'),
             board_id=create_data.get('board_id')
@@ -58,7 +64,7 @@ def get_tasks():
             database.db_session.commit()
         except Exception as error:
             return jsonify(error=str(error))
-        return jsonify(new_task.to_dict())
+        return jsonify(new_task.to_dict()), 201
 
 
 @app.get('/todo/api/v1.0/tasks/<int:id_task>')
@@ -76,7 +82,10 @@ def update_task(id_task):
     get_task = models.Task.query.filter_by(id=id_task).first()
     if not get_task:
         abort(404)
-    get_task.status = bool(request.get_json().get('status'))
+    new_status = request.get_json().get('status', None)
+    if not new_status:
+        abort(400)
+    get_task.status = bool(new_status)
     database.db_session.add(get_task)
     database.db_session.commit()
     return jsonify(get_task.to_dict())
